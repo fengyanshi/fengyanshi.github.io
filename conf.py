25025082cfb9174ff66db605531d58526381c1f5
Marissa Torres <mjtorres.usace@gmail.com>
2019-06-27 10:55:35 -0400

    added html theme option to include funwave logo

--- conf.py
+++ conf.py
@@ -105,7 +105,9 @@ html_theme = 'agogo'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+        logo:'Funwave_logo_only.jpg'
+        }
 
 html_theme_path = ['./build/html/_static']
 
